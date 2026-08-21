5beb1c424d541613305d6a09e97a5e20c1fc0aa4
gramdaheliyuvasamiti <daheliyuvasamiti@gmail.com>
2026-08-21 14:32:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -434,7 +434,7 @@ if (galleryUploadBtn) {
     for (const file of files) {
 
   const fileName =
-  Date.now() + "." + file.name.split('.').pop();
+  Date.now() + "_" + file.name;
 
   const { error } =
     await supabaseClient.storage
@@ -545,7 +545,7 @@ async function setupFileUpload(
     }
   
 const fileName =
-  Date.now() + "." + file.name.split('.').pop();
+  Date.now() + "_" + file.name;
 
     const { error } =
       await supabaseClient.storage
@@ -625,7 +625,7 @@ if (category === "Expenses") listId = "expensesList";
     item.className = "file-item";
 
     item.innerHTML = `
-      <span>${file.name}</span>
+     <span>${file.name.replace(/^\d+_/, '')}</span>
 
       <a
         class="download-btn"
